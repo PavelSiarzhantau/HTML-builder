d5a95927554b191928b31cfa2fa7622ac1a03baa
Pavel Siarzhantau <Pavel.Siarzhantau@gmail.com>
2022-05-23 13:30:00 +0300

    feat: complete "04-copy-file" task, (Mon May 23 2022 13:29:43 GMT+0300)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  createDir();
+  fs.readdir(
+    path.join(__dirname, 'files'),
+    { withFileTypes: false },
+    (err, files) => {
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(__dirname, `/files/${file}`),
+          path.join(__dirname, `/files-copy/${file}`),
+          fs.constants.COPYFILE_FICLONE,
+          (err) => {
+            if (err) console.log('Error found: ', err);
+          }
+        );
+        console.log(`${file} was successfully copied to folder 'files-copy'`);
+      });
+    }
+  );
+}
+function createDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Directory created successfully!');
+  });
+}
+copyDir();
