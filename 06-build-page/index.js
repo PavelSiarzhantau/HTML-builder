e380bb76d32e789e81053332c94fb16aff9b20de
Pavel Siarzhantau <Pavel.Siarzhantau@gmail.com>
2022-05-24 12:19:34 +0300

    feat: complete "06-build-page" task (Tue May 24 2022 12:18:58 GMT+0300)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,27 +3,62 @@ const fsPromises = fs.promises;
 const path = require('path');
 
 
-//assets helper
-let copyAssets = (dirname)=>{
-  fs.mkdir(path.join(__dirname,'/project-dist/assets'),{recursive:true},(err)=>{
-    if(err) return console.error(err);
+//assets logic
+//Clear
+let deleteAssets = (dirname)=>{
+  fs.access(path.join(__dirname,'/project-dist/assets'),fs.constants.F_OK,(err)=>{
+    if(err){
+      console.log('first bundle');
+    }else{
+      fs.readdir(path.join(__dirname, dirname),{withFileTypes: true},(err,files)=>{
+        if(err) console.log(err.message);
+        files.forEach((file)=>{
+          let finalDir;
+          if(!file.isFile()){
+            finalDir = dirname + `/${file.name}`;
+            deleteAssets(finalDir);
+          }else{
+            fs.unlink(path.join(__dirname,`${dirname}/${file.name}`),(err)=>{
+              if(err) console.log(err.message);
+            });
+          }
+        });
+      });
+    }
   });
-//   fs.readdir(path.join(__dirname,dirname),{withFileTypes:true},(err,files)=>{
-//     files.forEach((file)=>{
-//       if(! file.isDirectory){
-
-//       }else{
-//         fs.copyFile(path.join(__dirname,`/assets/${file.name}`),
-//           path.join(__dirname,`/project-dist/${file.name}`),
-//           fs.constants.COPYFILE_FICLONE,
-//           (err)=>{
-//             if(err) console.log('Error found: ', err);
-//           });
-//       }
-//     });
-//   });
-
 };
+//copy all Assets
+let copyAssets = (dirname)=>{
+  fs.access(path.join(__dirname,'/project-dist/assets'),fs.constants.F_OK,(err)=>{
+    if(err){
+      fs.mkdir(path.join(__dirname,'/project-dist/assets'),{recursive:true},(err)=>{
+        if(err) console.error(err);
+      });
+    }
+  });
+
+  fs.readdir(path.join(__dirname, dirname),{withFileTypes: true},(err,files)=>{
+    if(err) console.log(err.message);
+    files.forEach((file)=>{
+      let finalDir;
+      if(!file.isFile()){
+        finalDir = dirname + `/${file.name}`;
+        fs.mkdir(path.join(__dirname, `/project-dist/${finalDir}`),{recursive: true},(err)=>{
+          if(err) console.error(err.message);
+          copyAssets(finalDir);
+        });
+      }else{
+        fs.copyFile(path.join(__dirname, `${dirname}/${file.name}`),
+          path.join(__dirname,`/project-dist/${dirname}/${file.name}`),
+          fs.constants.COPYFILE_FICLONE,
+          (err) => {
+            if (err) console.log('Error found: ', err);
+          });
+      }
+    });
+  });
+};
+
 //css helpers
 fs.readdirAsyncCss = (dirname)=>{
   return new Promise((resolve,reject)=>{
@@ -121,6 +156,7 @@ createDir.then(fs.readdirAsync(path.join(__dirname, '/components/'))
         }
         return new Promise(res=>res());
       }).then(()=>{
+        deleteAssets('/project-dist/assets');
         copyAssets('assets');
       });
   }));
